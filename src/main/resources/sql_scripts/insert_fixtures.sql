7d5b75df47898827119e273a3278e101c03de2cd
Jan Usarek <jan.usarek@gmail.com>
2016-09-16 12:45:09 +0200

    Add descriptions to metadata fixtures

--- src/main/resources/sql_scripts/insert_fixtures.sql
+++ src/main/resources/sql_scripts/insert_fixtures.sql
@@ -2,11 +2,11 @@
 
 -- Commands fixures
 
-INSERT INTO traccar_api_metadata.device_models(id, imageUrl, deviceName)
-VALUES (1, 'datamatica.pl/images/lk210.png', 'LK210');
+INSERT INTO traccar_api_metadata.device_models(id, imageUrl, deviceName, descriptionShort, descriptionLong)
+VALUES (1, 'datamatica.pl/images/lk210.png', 'LK210', 'GPS motocyklowy', 'GPS motocyklowy, niewielki, łatwy do ukrycia, wodoszczelny');
 
-INSERT INTO traccar_api_metadata.command_types(id, commandName)
-VALUES (1, 'positionPeriodic');
+INSERT INTO traccar_api_metadata.command_types(id, commandName, description)
+VALUES (1, 'positionPeriodic', 'Jednokrotne zwrócenie aktualnej pozycji.');
 
 INSERT INTO traccar_api_metadata.command_types(id, commandName)
 VALUES (2, 'setSOSNumbers');
@@ -17,8 +17,8 @@ VALUES (1, 1);
 INSERT INTO traccar_api_metadata.devicemodel_commandtype(deviceModelId, commandTypeId)
 VALUES (1, 2);
 
-INSERT INTO traccar_api_metadata.command_parameters(id, parameterName, valueType, commandTypeId)
-VALUES (1, 'frequency', 'integer', 1);
+INSERT INTO traccar_api_metadata.command_parameters(id, parameterName, valueType, description, commandTypeId)
+VALUES (1, 'frequency', 'integer', 'Częstotliwość powiadamiana', 1);
 
 INSERT INTO traccar_api_metadata.command_parameters(id, parameterName, valueType, commandTypeId)
 VALUES (2, 'SOSNumber1', 'string', 2);
@@ -40,11 +40,11 @@ VALUES (2, 1);
 
 -- Report fixtures
 
-INSERT INTO traccar_api_metadata.report_types(id, reportName, imageUrl)
-VALUES (1, 'Jazda i postoje', 'www.datamatica.pl/images/jazda_postoje.png');
+INSERT INTO traccar_api_metadata.report_types(id, reportName, imageUrl, descriptionShort, descriptionLong)
+VALUES (1, 'Jazda i postoje', 'www.datamatica.pl/images/jazda_postoje.png', 'Raport z przebiegu trasy', 'Szczegółowy raport z przebiegu trasy wraz z zatrzymaniami');
 
-INSERT INTO traccar_api_metadata.report_parameters(id, parameterKey, parameterName, parameterValue, reportTypeId)
-VALUES (1, 'timePeriod', 'Obejmowany okres', 'doubleDate', 1);
+INSERT INTO traccar_api_metadata.report_parameters(id, parameterKey, parameterName, parameterValue, description, reportTypeId)
+VALUES (1, 'timePeriod', 'Obejmowany okres', 'doubleDate', 'Zakres dat wraz z godzinami', 1);
 
 INSERT INTO traccar_api_metadata.report_constraints(constraintKey, constraintValue, ReportParameterId)
 VALUES ('minDate', '2016-01-01', 1);
@@ -64,8 +64,8 @@ VALUES ('allowedModels', '["LK210", "LK209"]', 2);
 INSERT INTO traccar_api_metadata.report_types(id, reportName, imageUrl)
 VALUES (2, 'Informacje ogólne', 'www.datamatica.pl/images/informacje_ogolne.png');
 
-INSERT INTO traccar_api_metadata.report_parameters(id, parameterKey, parameterName, parameterValue, reportTypeId)
-VALUES (3, 'timePeriod', 'Obejmowany okres', 'doubleDate', 2);
+INSERT INTO traccar_api_metadata.report_parameters(id, parameterKey, parameterName, parameterValue, description, reportTypeId)
+VALUES (3, 'timePeriod', 'Obejmowany okres', 'doubleDate', 'Zakres dat wraz z godzinami', 2);
 
 INSERT INTO traccar_api_metadata.report_constraints(constraintKey, constraintValue, ReportParameterId)
 VALUES ('minDate', '2016-02-01', 3);
