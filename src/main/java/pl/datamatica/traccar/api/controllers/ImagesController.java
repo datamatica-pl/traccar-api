aadedd33d8e6da091657e5c818f487b8e53b655c
Piotr Krzeszewski <piotr.krzeszewski@datamatica.com.pl>
2016-10-27 13:51:09 +0200

    - Fixed path and added proper 404 response

--- src/main/java/pl/datamatica/traccar/api/controllers/ImagesController.java
+++ src/main/java/pl/datamatica/traccar/api/controllers/ImagesController.java
@@ -6,6 +6,7 @@
 package pl.datamatica.traccar.api.controllers;
 
 import java.io.FileInputStream;
+import org.hibernate.annotations.NotFound;
 import pl.datamatica.traccar.api.Application;
 import pl.datamatica.traccar.api.providers.ImageProvider;
 import spark.Request;
@@ -22,12 +23,20 @@ public class ImagesController extends ControllerBase{
 
         @Override
         public void bind() {
-            Spark.get(rootUrl()+"/:name", (req, res) -> {
+            Spark.get(baseUrl()+"/:name", (req, res) -> {
                 ImagesController ic = createController(req);
 
-                byte[] imageData = ic.getRawImageData(":name");
+                byte[] imageData = ic.getRawImageData(req.params(":name"));
+
+                if (imageData == null) {
+                    return gson.toJson(render(ic.notFound(), res));
+                }
+                
                 res.raw().setContentType("image/png;charset=utf-8");
+                // By using OutputStream maximum size of image if 64kB. 
+                // To provide bigger images data should be wrote in loop (max 64kB at once).
                 res.raw().getOutputStream().write(imageData, 0, imageData.length);
+                
                 return res;
             });
         }
@@ -38,9 +47,8 @@ public class ImagesController extends ControllerBase{
             return ic;
         }
         
-        @Override
-        public String rootUrl() {
-            return super.rootUrl() + "/images";
+        public String baseUrl() {
+            return resourcesUrl() + "/images";
         }
     }
     
--- src/main/java/pl/datamatica/traccar/api/providers/ImageProvider.java
+++ src/main/java/pl/datamatica/traccar/api/providers/ImageProvider.java
@@ -22,8 +22,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 public class ImageProvider {
