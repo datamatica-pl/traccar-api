e859269889fbbebfe72d2b7b1ef1d1d48d238678
Jan Usarek <janusarek@users.noreply.github.com>
2017-02-10 11:00:32 +0100

    Agts 267 nie dodawac alarmow do sciezki (#60)
    * Add baterry level info for GT06 and H02 protocols (position based).
    * Return positons with proper valid status only

--- src/main/java/pl/datamatica/traccar/api/controllers/DevicesController.java
+++ src/main/java/pl/datamatica/traccar/api/controllers/DevicesController.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright (C) 2016  Datamatica (dev@datamatica.pl)
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
  *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,12 +40,12 @@ import spark.Request;
 import spark.Spark;
 
 public class DevicesController extends ControllerBase {
-    
-    public static class Binder extends ControllerBinder { 
-        
+
+    public static class Binder extends ControllerBinder {
+
         @Override
         public void bind() {
-            Spark.get(rootUrl(), (req, res) -> { 
+            Spark.get(rootUrl(), (req, res) -> {
                 DevicesController dc = createController(req);
                 return render(dc.get(), res);
             }, gson::toJson);
@@ -55,23 +55,23 @@ public class DevicesController extends ControllerBase {
                 AddDeviceDto deviceDto = gson.fromJson(req.body(), AddDeviceDto.class);
                 return render(dc.post(deviceDto), res);
             }, gson::toJson);
-            
+
             Spark.put(rootUrl()+"/:id", (req, res) -> {
                 DevicesController dc = createController(req);
                 EditDeviceDto deviceDto = gson.fromJson(req.body(), EditDeviceDto.class);
                 return render(dc.put(Long.parseLong(req.params(":id")), deviceDto), res);
             }, gson::toJson);
 
-            Spark.get(rootUrl()+"/:id", (req, res) -> {            
+            Spark.get(rootUrl()+"/:id", (req, res) -> {
                 DevicesController dc = createController(req);
                 return render(dc.get(Long.parseLong(req.params(":id"))), res);
             }, gson::toJson);
-            
+
             Spark.delete(rootUrl() + "/:id", (req, res)-> {
                 DevicesController dc = createController(req);
                 return render(dc.delete(Long.parseLong(req.params(":id"))), res);
             }, gson::toJson);
-            
+
             Spark.get(rootUrl() + "/:id/positions", (req, res) -> {
                 DevicesController dc = createController(req);
                 return render(dc.getPositions(Long.parseLong(req.params(":id"))), res);
@@ -97,7 +97,7 @@ public class DevicesController extends ControllerBase {
             DevicesController dc = new DevicesController(context);
             return dc;
         }
-        
+
         @Override
         public String rootUrl() {
             return super.rootUrl() + "/devices";
@@ -108,14 +108,14 @@ public class DevicesController extends ControllerBase {
     private final DeviceProvider dp;
     private final PositionProvider positions;
     private final Date minDate;
-    
+
     public DevicesController(RequestContext requestContext) {
         super(requestContext);
         this.dp = requestContext.getDeviceProvider();
         this.positions = requestContext.getPositionProvider();
         this.minDate = requestContext.getModificationDate();
     }
-    
+
     public HttpResponse get() throws Exception {
         List<Device> devices = dp.getAllAvailableDevices()
                 .filter(d -> !d.isDeleted())
@@ -127,14 +127,14 @@ public class DevicesController extends ControllerBase {
         long[] deviceIds = devices.stream()
                 .mapToLong(d -> d.getId())
                 .toArray();
-        
+
         return new OkCachedResponse(new ListDto<>(changedDevices, deviceIds),
                 devices.stream()
                         .map(d -> d.getLastUpdate())
                         .max((d1, d2) -> d1.compareTo(d2))
                         .orElse(Application.EMPTY_RESPONSE_MODIFICATION_DATE));
     }
-    
+
     public HttpResponse get(long id) throws Exception {
         try{
             return okCached(new DeviceDto.Builder().device(dp.getDevice(id)).build());
@@ -142,13 +142,13 @@ public class DevicesController extends ControllerBase {
             return handle(e);
         }
     }
-    
+
     public HttpResponse post(AddDeviceDto deviceDto) throws Exception {
         List<ErrorDto> validationErrors = AddDeviceDto.validate(deviceDto);
         if(!validationErrors.isEmpty())
             return badRequest(validationErrors);
         try {
-            Device device = dp.createDevice(deviceDto.getImei()); 
+            Device device = dp.createDevice(deviceDto.getImei());
             return created("devices/"+device.getId(), new DeviceDto.Builder().device(device).build());
         } catch(ProviderException e) {
             switch(e.getType()) {
@@ -160,12 +160,12 @@ public class DevicesController extends ControllerBase {
             throw e;
         }
     }
-    
+
     public HttpResponse put(long id, EditDeviceDto deviceDto) throws ProviderException {
         List<ErrorDto> errors = EditDeviceDto.validate(deviceDto);
         if(!errors.isEmpty())
             return badRequest(errors);
-            
+
         try {
             dp.updateDevice(id, deviceDto);
             return ok("");
@@ -173,23 +173,27 @@ public class DevicesController extends ControllerBase {
             return handle(e);
         }
     }
-    
+
     public HttpResponse delete(long id) throws Exception {
         try {
             dp.delete(id);
             return ok("");
         } catch(ProviderException e) {
             return handle(e);
-        } 
+        }
     }
-    
+
     public HttpResponse getPositions(long id) throws Exception {
         try {
             Device device = dp.getDevice(id);
-            return okCached(new ListDto<PositionDto>(positions
-                    .getAllAvailablePositions(device, minDate, MAX_RESULT_COUNT+1)
-                    .map(p -> new PositionDto.Builder().position(p).build())
-                    .collect(Collectors.toList()), MAX_RESULT_COUNT));
+
+            return okCached(new ListDto<PositionDto>(
+                    positions
+                        .getAllAvailablePositions(device, minDate, MAX_RESULT_COUNT+1)
+                        .filter(position -> position.hasProperValidStatus())
+                        .map(p -> new PositionDto.Builder().position(p).build())
+                        .collect(Collectors.toList()),
+                    MAX_RESULT_COUNT));
         } catch (ProviderException ex) {
             return handle(ex);
         }
