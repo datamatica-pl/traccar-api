567e9e7964c2d27958f34ebcd656612e3367480f
Jan Usarek <jan.usarek@gmail.com>
2017-11-22 14:03:58 +0100

    Use specific exceptions instead of generic one in CommandService

--- src/main/java/pl/datamatica/traccar/api/services/CommandService.java
+++ src/main/java/pl/datamatica/traccar/api/services/CommandService.java
@@ -1,21 +1,22 @@
 /*
  *  Copyright (C) 2016  Datamatica (dev@datamatica.pl)
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
  *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package pl.datamatica.traccar.api.services;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +27,7 @@ public class CommandService {
     public Map<String, Object> sendCommand(Object activeDevice, Object backendCommand) {
         final Map<String, Object> result = new HashMap<>();
         final Object awaiter = new Object();
-        
+
         try {
             Method sendCommand = activeDevice.getClass().getDeclaredMethod("sendCommand",
                     backendCommand.getClass(), Object.class);
@@ -34,25 +35,27 @@ public class CommandService {
             synchronized (awaiter) {
                 awaiter.wait();
             }
-        } catch (Exception e) {
+        } catch (IllegalAccessException | IllegalArgumentException | InterruptedException
+                | NoSuchMethodException | SecurityException | InvocationTargetException e) {
             return null;
         }
-        
+
         return result;
     }
-    
+
     public Map<String, Object> sendCustomCommand(Object activeDevice, String msg) {
         final Map<String, Object> result = new HashMap<>();
         final Object awaiter = new Object();
-        
+
         try {
             Method sendCommand = activeDevice.getClass().getDeclaredMethod("write", Object.class,
-                                Object.class);
+                    Object.class);
             sendCommand.invoke(activeDevice, msg, new CommandHandler(result, awaiter));
-            synchronized(awaiter) {
+            synchronized (awaiter) {
                 awaiter.wait();
             }
-        } catch(Exception e) {
+        } catch (IllegalAccessException | IllegalArgumentException | InterruptedException
+                | NoSuchMethodException | SecurityException | InvocationTargetException e) {
             return null;
         }
         return result;
