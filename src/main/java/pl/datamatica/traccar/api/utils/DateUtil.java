4839ecdbe8a4b39fb3d5cbb77ba3dc930695d51f
janusarek <jan.usarek@gmail.com>
2018-11-26 17:56:51 +0100

    Allow time zone in RFC 1123 and RFC 850 date formats.

--- src/main/java/pl/datamatica/traccar/api/utils/DateUtil.java
+++ src/main/java/pl/datamatica/traccar/api/utils/DateUtil.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright (C) 2016  Datamatica (dev@datamatica.pl)
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
  *  by the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,17 +22,17 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class DateUtil {    
-    private static final SimpleDateFormat rfc1123 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.US);
-    private static final SimpleDateFormat rfc850 = new SimpleDateFormat("EEEE,  dd-MMM-yy HH:mm:ss 'GMT'", Locale.US);
-    private static final SimpleDateFormat asctime = new SimpleDateFormat("EEE MMM  d HH:mm:ss yyyy", Locale.US);
-    
+public class DateUtil {
+    private static final SimpleDateFormat RFC_1123 = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+    private static final SimpleDateFormat RFC_850 = new SimpleDateFormat("EEEE,  dd-MMM-yy HH:mm:ss z", Locale.US);
+    private static final SimpleDateFormat ASCTIME = new SimpleDateFormat("EEE MMM  d HH:mm:ss yyyy", Locale.US);
+
     static {
-        rfc1123.setTimeZone(TimeZone.getTimeZone("GMT"));
-        rfc850.setTimeZone(TimeZone.getTimeZone("GMT"));
-        asctime.setTimeZone(TimeZone.getTimeZone("GMT"));
+        RFC_1123.setTimeZone(TimeZone.getTimeZone("GMT"));
+        RFC_850.setTimeZone(TimeZone.getTimeZone("GMT"));
+        ASCTIME.setTimeZone(TimeZone.getTimeZone("GMT"));
     }
-    
+
     public static synchronized Date parseDate(String in) throws ParseException {
         ParseException lastException = null;
         for(SimpleDateFormat format : getSupportedFormats())
@@ -43,12 +43,12 @@ public class DateUtil {
             }
         throw lastException;
     }
-    
+
     public static synchronized String formatDate(Date in) {
-        return rfc1123.format(in);
+        return RFC_1123.format(in);
     }
-    
+
     private static SimpleDateFormat[] getSupportedFormats() {
-        return new SimpleDateFormat[]{rfc1123, rfc850, asctime};
+        return new SimpleDateFormat[]{RFC_1123, RFC_850, ASCTIME};
     }
 }
